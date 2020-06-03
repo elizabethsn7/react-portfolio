8d6f8685f0f1b801b236b7d5bb90046d0ada6c55
Elizabeth Kipp <liz@elizabethkipp.com>
2020-06-02 19:22:10 -0500

    Updates

---
+++ precache-manifest.a808c758b73ae764f5190c9dcf81b468.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d718150f3934bbbfd4be220fc5d867d8",
+    "url": "/elizabethsn7/react-portfolio/index.html"
+  },
+  {
+    "revision": "62dfd5f6218988a25462",
+    "url": "/elizabethsn7/react-portfolio/static/css/2.861a4379.chunk.css"
+  },
+  {
+    "revision": "3230b013571218554c2b",
+    "url": "/elizabethsn7/react-portfolio/static/css/main.c6ead0aa.chunk.css"
+  },
+  {
+    "revision": "62dfd5f6218988a25462",
+    "url": "/elizabethsn7/react-portfolio/static/js/2.bf72289f.chunk.js"
+  },
+  {
+    "revision": "e88f2b13fa9951d7bdd41825412dd8da",
+    "url": "/elizabethsn7/react-portfolio/static/js/2.bf72289f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3230b013571218554c2b",
+    "url": "/elizabethsn7/react-portfolio/static/js/main.3b3692cd.chunk.js"
+  },
+  {
+    "revision": "e248a501f1f7269b46cc",
+    "url": "/elizabethsn7/react-portfolio/static/js/runtime-main.8f91f6dd.js"
+  },
+  {
+    "revision": "1cabb9e5071d62a8b0768865edc3bfd7",
+    "url": "/elizabethsn7/react-portfolio/static/media/LizKipp.1cabb9e5.jpg"
+  },
+  {
+    "revision": "0b88d04f0f09cc63d9694dbecf1d55c5",
+    "url": "/elizabethsn7/react-portfolio/static/media/angular-quiz.0b88d04f.jpg"
+  },
+  {
+    "revision": "8d787ddb242b63bad5018edad434c078",
+    "url": "/elizabethsn7/react-portfolio/static/media/hero.8d787ddb.jpg"
+  },
+  {
+    "revision": "103a5557c6c31c0e4b7675b5ece60175",
+    "url": "/elizabethsn7/react-portfolio/static/media/meetup.103a5557.jpg"
+  },
+  {
+    "revision": "b7d29eb2bbaf2946c3799acc0523c03a",
+    "url": "/elizabethsn7/react-portfolio/static/media/myFlixHome.b7d29eb2.jpg"
+  },
+  {
+    "revision": "6654d099bfb7e9888a7b35a580b33f28",
+    "url": "/elizabethsn7/react-portfolio/static/media/vue-js.6654d099.jpg"
+  }
+]);
